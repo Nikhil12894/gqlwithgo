5a0aa28806084d9dcc3fdb979e2ace5ce36aa0dc
Nalin Kumar <38244351+Nikhil12894@users.noreply.github.com>
2021-05-30 11:19:07 +0530

    Initial commit

--- server.go
+++ server.go
@@ -27,19 +27,3 @@ func runApp() {
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(r.Run(":" + port))
 }
-
-// with mix
-/* func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
-
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
-
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
-
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
-} */
